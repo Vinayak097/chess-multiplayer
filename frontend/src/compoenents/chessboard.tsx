f551c7a3890c14e63b25c70db5bdf756c66375b7
unknown <vinayak20injamure@gmail.com>
2024-06-20 22:30:24 +0530

    m

--- frontend/src/compoenents/chessboard.tsx
+++ frontend/src/compoenents/chessboard.tsx
@@ -1,14 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Chess, Color, PieceSymbol, Square } from 'chess.js'
+import { json } from 'react-router-dom'
+const chess=new Chess()
+const Chessboard=({board, socket}:{
+  board:({
+    square:Square,
+    type:PieceSymbol,
+    color:Color
 
-function Chessboard() {
+  }|null)[][], 
+  socket:WebSocket;
+
+}) =>{  
+  console.log(board)
+
+  const [from ,setform]  =useState<null|Square>()
+  const [to,setTo]=useState<null|Square>()
   return (
-    <div>
-        <div>
-            chessBaord
-        </div>
+    <div  className= '  text-white p-2 w-full '>
+      <div>
+      </div>
+        {board.map((row,i)=>{
+          return <div  key={i} className='flex justify-center '>
+            {row.map((square,j)=>{
+              const squareRepresantation=String.fromCharCode(65+(j%8))+""+(8-1) as Square;
+              return <div onClick={()=>{
+                if(!from){
+                  setform(squareRepresantation)
+                }else{
+                 console.log(square)
+                  socket?.send(JSON.stringify({
+                    type:"move",
+                    payload:{
+                      move:{
+                        from,
+                      to:squareRepresantation
+
+                      }
+                      
+                    }
+                  }))
+                  setform(null)
+                  console.log(from,squareRepresantation)                  
+                }
+                
+              }} key={j} className={`flex items-center justify-center w-16 h-16 bg-green-500 ${(i+j)%2==0?"bg-blue-50":""} ${square?.color=="w"?"text-black":"text-amber-900 " }`}>{square? square.type:" "}</div>
+            })}
+          </div>
+        })}
+        
+      
+
 
     </div>
   )
 }
 
-export default Chessboard
+export default Chessboard;
--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Chessboard from '../compoenents/chessboard'
+import { useSocket } from '../hooks/useSocket'
+import { Chess } from 'chess.js';
+export const init_game="init_game";
+export const Move="move";
+export const game_over="game_over"
 function Game() {
+  const socket=useSocket();
+  const [chess ,setChess]=useState(new Chess())
+  const [board,setBoard]=useState(chess.board())
+  useEffect(()=>{
+    if(socket){
+      socket.onmessage=(event)=>{
+        const message=JSON.parse(event.data);
+        console.log(message)
+        switch(message.type){
+          case init_game:
+            setChess(new Chess())
+            setBoard(chess.board())
+            console.log("game initiated")
+            break;
+          case Move:
+            const move=message.payload;
+            chess.move(move)
+            setBoard(chess.board())
+            console.log("Move made")
+            break;
+          case game_over:
+            console.log("game over")
+            break;
+        }
+      }
+    }
+  })
   return (
     <div>
 
     
-    <div className='flex justify-center border'>
-      <div className='pt-8 max-w-screen-lg w-full '>
+    <div className='flex justify-center bg-slate-900 h-screen  '>
+      <div className='pt-8 max-w-screen-lg  w-full '>
         <div className='grid grid-cols-6 gap-4  '>
-        <div className='col-span-4  bg-red-200 w-full'>
-          <Chessboard></Chessboard>
+        <div className='  col-span-3  bg-slate-700  w-full'>
+          <Chessboard board={board}></Chessboard>
         </div>
-        <div className='col-span-2 bg-green-200 w-full' >
-          <button>Play </button>
+        <div className='col-span-2 bg-slate-700 text-white w-full flex flex-col gap-4 items-center  rounded-md' >
+          <select name="timstamp" id="timestamp" className='mt-8 w-48 p-2 bg-slate-500 text-white  '>time 00</select>
+          <button className='bg-green-500 hover:bg-green-600 p-2 w-48 rounded-md'>Play</button>
           
         </div>
         </div>
