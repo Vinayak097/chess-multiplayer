70684570b9734a824f8eed8b5079df6669727d6f
vinayak097 <vinayak20injamure@gmail.com>
2026-08-08 17:54:15 +0530

    vite production url setups

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,4 +1,9 @@
-
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000");
+const socketUrl =
+  import.meta.env.VITE_SOCKET_URL ||
+  (import.meta.env.PROD
+    ? "https://chess-multiplayer-lmrj.onrender.com"
+    : "http://localhost:3000");
+
+export const socket = io(socketUrl, { transports: ["websocket"] });
